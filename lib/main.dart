c1392b03bec685e3cde75557965680978190b0b7
uemirhanselim <uemirhanselim@gmail.com>
2023-01-23 19:43:01 +0300

    with camera plugin, camera works now

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:image_to_text/view/home_view.dart';
 
-void main() {
+late List<CameraDescription> _cameras;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras();
   runApp(const MyApp());
 }
 
@@ -15,5 +18,50 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Image To Text',
       home: HomeView(),
     );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  State<HomeView> createState() => HomeViewState();
+}
+
+class HomeViewState extends State<HomeView> {
+  late CameraController controller;
+  @override
+  void initState() {
+    controller = CameraController(_cameras[0], ResolutionPreset.max);
+    controller.initialize().then((value) {
+      if (mounted) return;
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller.value.isInitialized) {
+      return Container(color: Colors.greenAccent);
+    } else {
+      return CameraPreview(controller);
+    }
   }
 }
