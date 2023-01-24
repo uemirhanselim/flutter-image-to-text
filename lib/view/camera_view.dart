e1bbce0c1535917ed417194417b4309f8bae3eba
uemirhanselim <uemirhanselim@gmail.com>
2023-01-24 14:56:37 +0300

    now it can split words and meanings

--- lib/view/camera_view.dart
+++ lib/view/camera_view.dart
@@ -1,11 +1,15 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 
 class CameraView extends StatefulWidget {
   const CameraView({Key? key, required this.cameras}) : super(key: key);
+
   final List<CameraDescription>? cameras;
+
   @override
   State<CameraView> createState() => CameraViewState();
 }
@@ -13,9 +17,19 @@ class CameraView extends StatefulWidget {
 class CameraViewState extends State<CameraView> {
   late CameraController controller;
   XFile? pictureFile;
+  bool textScanning = false;
+  String scannedText = '';
+  late final TextRecognizer textRecognizer;
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
+    textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
     controller = CameraController(widget.cameras![0], ResolutionPreset.max);
     controller.initialize().then((_) {
       if (!mounted) {
@@ -26,10 +40,28 @@ class CameraViewState extends State<CameraView> {
     super.initState();
   }
 
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+  void getRecognisedText(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final textDetector = GoogleMlKit.vision.textRecognizer();
+    RecognizedText recognizedText = await textDetector.processImage(inputImage);
+    await textDetector.close();
+    scannedText = "";
+    List<String> words = [];
+    List<String> meanings = [];
+    for (TextBlock block in recognizedText.blocks) {
+      for (TextLine line in block.lines) {
+        String stext = "$scannedText${line.text}\n";
+        int index = stext.indexOf("=");
+        String first = stext.substring(0, index).trim();
+        String second = stext.substring(index + 1).trim();
+        words.add(first);
+        meanings.add(second);
+        log("WORDS: $words");
+        log("MEANINGS: $meanings");
+      }
+    }
+    textScanning = false;
+    setState(() {});
   }
 
   @override
@@ -41,35 +73,47 @@ class CameraViewState extends State<CameraView> {
         ),
       );
     }
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: SizedBox(
-              height: 400,
-              width: 400,
-              child: CameraPreview(controller),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Center(
+                child: SizedBox(
+                  height: 400,
+                  width: 400,
+                  child: CameraPreview(controller),
+                ),
+              ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: ElevatedButton(
+                  onPressed: () async {
+                    pictureFile = await controller.takePicture();
+                    textScanning = true;
+                    setState(() {});
+                    getRecognisedText(pictureFile!);
+                  },
+                  child: const Text("Capture Image")),
+            ),
+            pictureFile != null
+                ? Image.file(
+                    File(pictureFile!.path),
+                    width: 600,
+                    height: 200,
+                  )
+                : const SizedBox.shrink(),
+            Container(
+              child: Text(
+                scannedText,
+                style: const TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.all(8),
-          child: ElevatedButton(
-              onPressed: () async {
-                pictureFile = await controller.takePicture();
-                setState(() {});
-              },
-              child: const Text("Capture Image")),
-        ),
-        pictureFile != null
-            ? Image.file(
-                File(pictureFile!.path),
-                width: 600,
-                height: 200,
-              )
-            : const SizedBox.shrink(),
-      ],
+      ),
     );
   }
 }
